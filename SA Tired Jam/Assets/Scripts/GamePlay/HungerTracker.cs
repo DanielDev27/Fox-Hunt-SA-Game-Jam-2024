a79bc674ced32b5dd3c21035efea1fc4776020fe
DanielDev27 <danielbeale27@gmail.com>
2024-10-21 11:08:32 +0200

    Update HungerTracker.cs
    Lose condition

--- SA Tired Jam/Assets/Scripts/GamePlay/HungerTracker.cs
+++ SA Tired Jam/Assets/Scripts/GamePlay/HungerTracker.cs
@@ -5,15 +5,30 @@ public class HungerTracker : MonoBehaviour
     public static HungerTracker instance;
     public float foodStorage;
     [SerializeField] float hunger = 0;
+    [SerializeField] bool gameActive = true;
     [Header("UI References")]
     [SerializeField] TMP_Text hungerAmount;
 
     private void Awake()
     {
         instance = this;
+        gameActive = true;
     }
     private void Start()
     {
         hungerAmount.text = "Hunger: " + hunger;
+    }
+
+    private void Update()
+    {
+        if (hunger > 0)
+        {
+            hunger -= Time.deltaTime;
+        }
+        if (gameActive)
+        {
+            gameActive = false;
+            
+        }
     }
 }
