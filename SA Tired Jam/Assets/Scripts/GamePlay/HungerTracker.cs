4e60ae8d296d34d4eeb4d96514048fcbfe3d9abc
DanielDev27 <danielbeale27@gmail.com>
2024-10-21 09:33:35 +0200

    HUD and Hunger

--- SA Tired Jam/Assets/Scripts/GamePlay/HungerTracker.cs
+++ SA Tired Jam/Assets/Scripts/GamePlay/HungerTracker.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
-
+using TMPro;
 public class HungerTracker : MonoBehaviour
 {
     public static HungerTracker instance;
     public float foodStorage;
-    [SerializeField] float hunger;
+    [SerializeField] float hunger = 0;
+    [Header("UI References")]
+    [SerializeField] TMP_Text hungerAmount;
 
     private void Awake()
     {
         instance = this;
+    }
+    private void Start()
+    {
+        hungerAmount.text = "Hunger: " + hunger;
     }
 }
